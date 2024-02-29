d1f3266055a6393fea61fc6f75cd56837002ecab
singha55162326 <63039299+singha55162326@users.noreply.github.com>
2024-02-29 10:17:18 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
         {loading ? (
           // Display loading spinner or any other loading indicator
           <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-white bg-opacity-75 z-50">
-            <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500"></div>
+<svg xmlns="http://www.w3.org/2000/svg" width="256" height="256" viewBox="0 0 512 512"><defs><symbol id="meteoconsLightningBolt0" viewBox="0 0 96 176"><path fill="#f6a823" d="M32 0L0 96h32l-16 80L96 64H48L80 0H32z"><animate id="meteoconsLightningBolt1" attributeName="opacity" begin="0s; x1.end+.67s" dur="1.33s" keyTimes="0; .38; .5; .63; .75; .86; .94; 1" values="1; 1; 0; 1; 0; 1; 0; 1"/></path></symbol></defs><use width="96" height="176" href="#meteoconsLightningBolt0" transform="translate(190.13 132.72)scale(1.36)"/></svg>
           </div>
         ) : (
           // Render your content when loading is complete
