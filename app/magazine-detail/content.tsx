362016fed2fb9f736e704b359c6a70cf531863a4
singha55162326 <63039299+singha55162326@users.noreply.github.com>
2024-02-29 11:01:51 +0700

    Update content.tsx

--- app/magazine-detail/content.tsx
+++ app/magazine-detail/content.tsx
@@ -1,38 +1,42 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Content = () => {
   return (
-    <div>
+    <div className=' container   mx-auto'>
 
-<div class="flex mt-[5%] flex-wrap justify-center space-x-20 ">
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-20 mt-[5%]">
 
-  <div class="w-[200px]  transition-all hover:scale-110 h-[339px]  ">
-    <div class=" rounded-lg p-6 ">
+  <div className="w-[200px]  transition-all hover:scale-110 h-[339px]  ">
+    <Link href="/comingsoon">
+    <div className=" rounded-lg p-6 ">
    <img src='magazine/image 109.png'/>
-   <span className=' font-semibold'>LAO TRADE MAGAZINE,Lao Magazine, Advertising Lao Trade .....</span>
+   <span className=' font-semibold line-clamp-3'>LAO TRADE MAGAZINE,Lao Magazine, Advertising Lao Trade .....</span>
     </div>
+    </Link>
+  
   </div>
 
 
-  <div class="w-[200px]  transition-all hover:scale-110 h-[339px]">
-    <div class=" rounded-lg p-6 ">
+  <div className="w-[200px]  transition-all hover:scale-110 h-[339px]">
+    <div className=" rounded-lg p-6 ">
     <img src='magazine/image 112.png'/>
-   <span className=' font-semibold'>Visit Laos Magazine Vol.5 by Top Value Service Co., LTD Magazine</span>
+   <span className=' font-semibold line-clamp-3'>Visit Laos Magazine Vol.5 by Top Value Service Co., LTD Magazine</span>
     </div>
   </div>
 
-  <div class="w-[200px]  transition-all hover:scale-110 h-[339px ">
-    <div class=" rounded-lg p-6 ">
+  <div className="w-[200px]  transition-all hover:scale-110 h-[339px ">
+    <div className=" rounded-lg p-6 ">
     <img src='magazine/image 110.png'/>
-   <span className=' font-semibold'>Sabaidee - Lao lifestyle & travel magazine issue 26e</span>
+   <span className=' font-semibold line-clamp-3'>Sabaidee - Lao lifestyle & travel magazine issue 26e</span>
     </div>
   </div>
 
 
-  <div class="w-[200px]  transition-all hover:scale-110 h-[339px">
-    <div class=" rounded-lg p-6 ">
+  <div className="w-[200px]  transition-all hover:scale-110 h-[339px">
+    <div className=" rounded-lg p-6 ">
     <img src='magazine/image 111.png'/>
-   <span className=' font-semibold'>New Lao Guide Magazine Hits the Shelves | J&C Group - Your Trusted.......</span>
+   <span className=' font-semibold line-clamp-3'>New Lao Guide Magazine Hits the Shelves | J&C Group - Your Trusted.......</span>
     </div>
   </div>
 
@@ -50,35 +54,35 @@ const Content = () => {
 
 </div>
 
-<div class="flex mt-[5%] flex-wrap  justify-center  space-x-20">
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-20 mt-[5%]">
 
-  <div class="w-[200px]  transition-all hover:scale-110 h-[339px]  ">
-    <div class=" rounded-lg p-6 ">
+  <div className="w-[200px]  transition-all hover:scale-110 h-[339px]  ">
+    <div className=" rounded-lg p-6 ">
    <img src='magazine/Group 393.png'/>
-   <span className=' font-semibold'>Promo [90% Off] Dao Chay Hotel Laos | Hotel 1620 Promo Code</span>
+   <span className=' font-semibold line-clamp-3'>Promo [90% Off] Dao Chay Hotel Laos | Hotel 1620 Promo Code</span>
     </div>
   </div>
 
 
-  <div class="w-[200px]  transition-all hover:scale-110 h-[339px]">
-    <div class=" rounded-lg p-6 ">
+  <div className="w-[200px]  transition-all hover:scale-110 h-[339px]">
+    <div className=" rounded-lg p-6 ">
     <img src='magazine/image 114.png'/>
-   <span className=' font-semibold'>Visit Laos Magazine issue#6 by TopValueService................</span>
+   <span className=' font-semibold line-clamp-3'>Visit Laos Magazine issue#6 by TopValueService................</span>
     </div>
   </div>
 
-  <div class="w-[200px]  transition-all hover:scale-110 h-[339px ">
-    <div class=" rounded-lg p-6 ">
+  <div className="w-[200px]  transition-all hover:scale-110 h-[339px ">
+    <div className=" rounded-lg p-6 ">
     <img src='magazine/image 115.png'/>
-   <span className=' font-semibold'>Laoconnection.com: Free Lao Magazines Online</span>
+   <span className=' font-semibold line-clamp-3'>Laoconnection.com: Free Lao Magazines Online</span>
     </div>
   </div>
 
 
-  <div class="w-[200px]  transition-all hover:scale-110 h-[339px">
-    <div class=" rounded-lg p-6 ">
+  <div className="w-[200px]  transition-all hover:scale-110 h-[339px">
+    <div className=" rounded-lg p-6 ">
     <img src='magazine/image 116.png'/>
-   <span className=' font-semibold'>Visit Laos Magazine Vol.3 by Top Value Service Co., LTD Magazine.................</span>
+   <span className=' font-semibold line-clamp-3'>Visit Laos Magazine Vol.3 by Top Value Service Co., LTD Magazine.................</span>
     </div>
   </div>
 
@@ -96,97 +100,7 @@ const Content = () => {
 
 </div>
 
-<div class="flex mt-[5%] flex-wrap  justify-center space-x-20 ">
 
-  <div class="w-[200px]  transition-all hover:scale-110 h-[339px]  ">
-    <div class=" rounded-lg p-6 ">
-   <img src='magazine/image 109.png'/>
-   <span className=' font-semibold'>LAO TRADE MAGAZINE,Lao Magazine, Advertising Lao Trade .....</span>
-    </div>
-  </div>
-
-
-  <div class="w-[200px]  transition-all hover:scale-110 h-[339px]">
-    <div class=" rounded-lg p-6 ">
-    <img src='magazine/image 112.png'/>
-   <span className=' font-semibold'>Visit Laos Magazine Vol.5 by Top Value Service Co., LTD Magazine</span>
-    </div>
-  </div>
-
-  <div class="w-[200px]  transition-all hover:scale-110  h-[339px ">
-    <div class=" rounded-lg p-6 ">
-    <img src='magazine/image 110.png'/>
-   <span className=' font-semibold'>Sabaidee - Lao lifestyle & travel magazine issue 26e</span>
-    </div>
-  </div>
-
-
-  <div class="w-[200px]  transition-all hover:scale-110 h-[339px">
-    <div class=" rounded-lg p-6 ">
-    <img src='magazine/image 111.png'/>
-   <span className=' font-semibold'>New Lao Guide Magazine Hits the Shelves | J&C Group - Your Trusted.......</span>
-    </div>
-  </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-</div>
-
-<div class="flex mt-[5%] flex-wrap  justify-center  space-x-20">
-
-  <div class="w-[200px]  transition-all hover:scale-110 h-[339px]  ">
-    <div class=" rounded-lg p-6 ">
-   <img src='magazine/Group 393.png'/>
-   <span className=' font-semibold'>Promo [90% Off] Dao Chay Hotel Laos | Hotel 1620 Promo Code</span>
-    </div>
-  </div>
-
-
-  <div class="w-[200px]  transition-all hover:scale-110 h-[339px]">
-    <div class=" rounded-lg p-6 ">
-    <img src='magazine/image 114.png'/>
-   <span className=' font-semibold'>Visit Laos Magazine issue#6 by TopValueService................</span>
-    </div>
-  </div>
-
-  <div class="w-[200px]  transition-all hover:scale-110 h-[339px ">
-    <div class=" rounded-lg p-6 ">
-    <img src='magazine/image 115.png'/>
-   <span className=' font-semibold'>Laoconnection.com: Free Lao Magazines Online</span>
-    </div>
-  </div>
-
-
-  <div class="w-[200px]  transition-all hover:scale-110 h-[339px">
-    <div class=" rounded-lg p-6 ">
-    <img src='magazine/image 116.png'/>
-   <span className=' font-semibold'>Visit Laos Magazine Vol.3 by Top Value Service Co., LTD Magazine.................</span>
-    </div>
-  </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-</div>
 <div
             className="wow flex align-bottom fadeInUp top-0 -mx-4 "
             data-wow-delay=".15s"
